7b563bf5dd5d25b6533f0baabaebdaeaeabc4291
Alexey Kirpichnikov <masonry1@yandex.ru>
2020-04-13 18:40:41 +0800

    Initial Commit

---
+++ PeliculaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeliculaApp
+//
+//  Created by Лена Мырленко on 2020/4/13.
+//  Copyright © 2020 Surf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
